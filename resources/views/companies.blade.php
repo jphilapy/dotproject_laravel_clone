6572d62096deb37dcb31b7daef5b248bc0084bd9
Jeff Philapy <jeff@altahost.com>
2022-10-29 14:19:58 -0400

    Adding customer model, livewire component, basic page layout

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -4,7 +4,22 @@
             {{ __('Companies') }}
         </h2>
     </x-slot>
-
-    <livewire:companies-sub-menu />
-
+    <div>
+        <livewire:companies-sub-menu/>
+    </div>
+    <!-- main sections -->
+    <main id="mainSections" class="main-sections py-4">
+        <!-- page content -->
+        <section class="page-content">
+            <div class="container-fluid">
+                <div class="card card-body p-4">
+                    <div>
+                        <livewire:companies-list/>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- !page content -->
+    </main>
+    <!-- !main sections -->
 </x-app-layout>
---
+++ resources/views/livewire/companies-list.blade.php
@@ -0,0 +1,131 @@
+<div>
+    <div
+        class="d-flex flex-wrap align-items-center justify-content-end mb-3"
+    >
+        <a href="#" role="button" class="btn btn-success">
+            <i class="fa-solid fa-circle-plus"></i>
+            <span class="ms-2 d-none d-sm-inline">Add Company</span>
+        </a>
+        <form class="d-flex" role="search">
+            <div class="ms-3 search-box">
+                <input
+                    type="text"
+                    class="form-control bg-secondary bg-opacity-10"
+                    placeholder="Search"
+                    aria-label="Search"
+                    aria-describedby="Search box"
+                />
+            </div>
+        </form>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-hover align-middle text-nowrap">
+            <thead class="align-middle">
+            <tr>
+
+                <th scope="col">
+                    <a href="#" class="btn p-0" role="button">
+                        <span class="me-2">Name</span>
+                        <i class="fa-solid fa-sort"></i>
+                    </a>
+                </th>
+                <th scope="col">
+                    <a href="#" class="btn p-0" role="button">
+                        <span class="me-2">Address</span>
+                        <i class="fa-solid fa-sort"></i>
+                    </a>
+                </th>
+                <th scope="col">
+                    <a href="#" class="btn p-0" role="button">
+                        <span class="me-2">Website</span>
+                        <i class="fa-solid fa-sort"></i>
+                    </a>
+                </th>
+                <th scope="col">
+                    <a href="#" class="btn p-0" role="button">
+                        <span class="me-2">Email</span>
+                        <i class="fa-solid fa-sort"></i>
+                    </a>
+                </th>
+                <th scope="col">
+                    <a href="#" class="btn p-0" role="button">
+                        <span class="me-2">Phone</span>
+                        <i class="fa-solid fa-sort"></i>
+                    </a>
+                </th>
+                <th scope="col">
+                    <a href="#" class="btn p-0" role="button">
+                        <span class="me-2">Mobile</span>
+                        <i class="fa-solid fa-sort"></i>
+                    </a>
+                </th>
+                <th scope="col">
+                    <a href="#" class="btn p-0" role="button">
+                        <span class="me-2">Action</span>
+                        <i class="fa-solid fa-sort"></i>
+                    </a>
+                </th>
+            </tr>
+            </thead>
+            <tbody class="table-group-divider">
+            <tr>
+
+                <td>
+                    <a
+                        href="/customer/id"
+                        class="link-dark link:hover-warning"
+                    >
+                        <span class="text"> John Doe </span>
+                    </a>
+                </td>
+                <td>1112, N Street</td>
+                <td>
+                    <a
+                        href="https://johndoe.com"
+                        class="link-dark link:hover-warning"
+                    >
+                        <span class="text"> johndoe.com </span>
+                    </a>
+                </td>
+                <td>
+                    <a
+                        href="mailto:johndoe@domain.com"
+                        class="link-dark link:hover-warning"
+                    >
+                        <span class="text"> johndoe@domain.com </span>
+                    </a>
+                </td>
+                <td>
+                    <a
+                        href="tel:++777-777-7777"
+                        class="link-dark link:hover-warning"
+                    >
+                        <span class="text"> 777-777-7777 </span>
+                    </a>
+                </td>
+                <td>
+                    <a
+                        href="tel:++777-777-7777"
+                        class="link-dark link:hover-warning"
+                    >
+                        <span class="text"> 777-777-7777 </span>
+                    </a>
+                </td>
+                <td>
+                    <a
+                        href="#"
+                        class="btn btn-outline-secondary"
+                        role="button"
+                    >
+                        <i class="fa-regular fa-eye"></i>
+                    </a>
+                    <a href="#" class="btn btn-success ms-1" role="button">
+                        <i class="fa-regular fa-pen-to-square"></i>
+                    </a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
